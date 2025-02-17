9e299f198508c6a3e0ec057c0b59798b1b41b24a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-17 16:52:15 +0400

    using props and disabling eslint react/prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,60 @@
-// import React from "react";
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import "./App.css";
-
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const Search = () => {
   const handleChange = (event) => {
     // synthetic event
-    console.log(event);
+    // console.log(event);
     // value of target (here: input HTML element)
     console.log(event.target.value);
   };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input
-        id="search"
-        type="text"
-        // onChange={handleChange}
-        onBlur={handleChange}
-      />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 };
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const List = (props) => (
+    <ul>
+      {props.list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <ul>
-        <ul>
-          {list.map(function (item) {
-            return (
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </ul>
+      <List list={stories} />
     </div>
   );
 }
