dec272d21da22c02a3648a136e498692a36f55fd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-17 13:28:02 +0400

    when React can use no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import React from "react";
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
+function getTitle(title) {
+  return title;
+}
+
 const title = "React";
+
 function App() {
   // const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello {title}</h1>
-      <label htmlFor="input">My input: </label>
-      <input id="input"></input>
+      <h1>Hello {getTitle("React")}</h1>
+      {React.createElement("h1", null, `Hello from ${title}`)}
+      {React.createElement("h1", { id: 1 }, `Hello from ${title}`)}
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
