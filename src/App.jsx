47b3a7259b01597aa7af1bdf0f3d607bad2dec2f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-17 13:51:44 +0400

    lists and map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+// import React from "react";
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
-function getTitle(title) {
-  return title;
-}
-
-const title = "React";
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   // const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
-      {React.createElement("h1", null, `Hello from ${title}`)}
-      {React.createElement("h1", { id: 1 }, `Hello from ${title}`)}
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr />
+      <ul>
+        <ul>
+          {list.map(function (item) {
+            return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </ul>
     </div>
   );
 }
