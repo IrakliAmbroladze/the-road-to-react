8056015b414d94199faa6a074a165b55218cb73b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-17 16:31:25 +0400

    add event listener

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,27 @@ const list = [
   },
 ];
 
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
 function App() {
   // const [count, setCount] = useState(0);
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
       <hr />
       <ul>
         <ul>
