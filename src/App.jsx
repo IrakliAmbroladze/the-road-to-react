33797cf44029c832d5b8990c5882342e64ceea65
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-18 12:52:14 +0400

    React Props
    In this part i made List component and Item component apart from the main App body

--- src/App.jsx
+++ src/App.jsx
@@ -35,20 +35,23 @@ function App() {
 
   const List = (props) => (
     <ul>
-      {props.list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
     </ul>
   );
+
+  const Item = (props) => (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
